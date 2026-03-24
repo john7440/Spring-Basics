e5bb5fb819b53bb33ccdcbeb2a12961b85b0d2d0
Jonathan M. <neodev7440@gmail.com>
2026-03-24 09:27:04 +0100

    adding the menu display

--- src/main/java/fr/fms/ihm/ShopIHM.java
+++ src/main/java/fr/fms/ihm/ShopIHM.java
@@ -14,4 +14,27 @@ public class ShopIHM {
 
     private Scanner scanner = new Scanner(System.in);
 
+    public void start(){
+        int choice = -1;
+    }
+
+    private void printMenu(){
+        System.out.println("\nBienvenu dans notre application de gestion d'articles !");
+        System.out.println("1: Afficher tous les articles sans pagination");
+        System.out.println("2: Afficher tous les articles avec pagination");
+        System.out.println("***************************");
+        System.out.println("3: Ajouter un article");
+        System.out.println("4: Afficher un article");
+        System.out.println("5: Supprimer un article");
+        System.out.println("6: Modifier un article");
+        System.out.println("***************************");
+        System.out.println("7: Ajouter une categorie");
+        System.out.println("8: Afficher une categorie");
+        System.out.println("9: Supprimer une categorie");
+        System.out.println("10: Mettre a jour une categorie");
+        System.out.println("11: Afficher tous les articles d'une categorie");
+        System.out.println("***************************");
+        System.out.println("12: Sortir du programme");
+    }
+
 }
